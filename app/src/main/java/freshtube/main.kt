999d7b8222bbd87307dfec91072d7291c5cec246
gencodyybot <132603152+gencodyybot@users.noreply.github.com>
2023-05-10 16:37:30 -0400

    Update main.kt

--- app/src/main/java/freshtube/main.kt
+++ app/src/main/java/freshtube/main.kt
@@ -1,1 +1,38 @@
+package com.example.freshtube
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FreshtubeApp() {
+    MaterialTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            Column(modifier = Modifier.fillMaxSize()) {
+                // Top app bar
+                TopAppBar(title = { Text("FreshTube") })
+
+                // Main content
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    // Placeholder text for now
+                    Text("Hello, FreshTube!")
+                }
+            }
+        }
+    }
+}
 
